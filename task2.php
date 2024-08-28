1da3aa3d69cfbc438784b6f05990493ca4d41e44
Burhan-crypt <burkhan@scopenine.com>
2024-08-28 17:33:29 +0500

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,10 @@
+<?php
+ // //For Loops in PHP
+echo "This is For Loop in Action <br>";
+$sum = 0;
+    for ($a=1; $a <=50 ; $a++) { 
+        $sum += $a;
+    } 
+ echo "The sum of first 50 Natural Numbers is: <br> $sum"  
+
+?>  
