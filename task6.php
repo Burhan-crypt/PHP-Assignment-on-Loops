5818925a53da5c5f760a11e50ce01a24e4cb7057
Burhan-crypt <burkhan@scopenine.com>
2024-08-28 19:05:54 +0500

    Create task6.php

---
+++ task6.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h3> Addition of number of series using For Loop</h3>";
+$sum = 0;
+for ($a=1; $a <=4 ; $a++) { 
+    $sum += $a;
+}
+echo "The Sum of Numbers from 1 to 4: <br> $sum";
+?>
