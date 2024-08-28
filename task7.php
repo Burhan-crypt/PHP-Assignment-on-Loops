1ff36566cb8793f15799291102bdf423f0538211
Burhan-crypt <burkhan@scopenine.com>
2024-08-28 19:20:32 +0500

    Create task7.php

---
+++ task7.php
@@ -0,0 +1,11 @@
+<?php
+// Factorial of 5 using For Loop
+$number = 5;
+$factorial = 1;
+
+for ($a=1; $a <=$number ; $a++) { 
+    $factorial *=$a;
+}
+
+echo "The Factorial of 5 is: <br> $factorial";
+?>
