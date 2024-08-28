704f44528357cd9ec9aaf10ecd0dbb266f2aac55
Burhan-crypt <burkhan@scopenine.com>
2024-08-28 19:59:23 +0500

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,6 +1,6 @@
 <?php
 //For Loops in PHP
-    echo "This is For Loop in Action <br>";
+    echo "This is For Loop in Action for Even Numbers <br>";
     for ($for=2; $for <=20; $for+=2) { 
         echo "The Value of for is $for <br>";
     }
