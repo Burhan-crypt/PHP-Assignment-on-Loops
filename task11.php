daf3d09dbfbaddc7ca9b74c0ef50c5d91faa45c9
Burhan-crypt <burkhan@scopenine.com>
2024-09-10 20:11:58 +0500

    Update task11.php

--- task11.php
+++ task11.php
@@ -1,11 +1,12 @@
 <?php
-$str = "Hello World";
+$str = "hello world";
 $vowels = ['a', 'e', 'i', 'o', 'u'];
 $count = 0;
-        for ($a=0; $a < strlen($str) ; $a++) {       /*strlen is a function in PHP*/
-            if (in_array($str[$a], $vowels)) {     /*in_array is also a function of PHP*/
-                $count++;
-            }
-        }
-        echo "The number of vowels in $str are: " . $count;
+
+for ($a = 0; $a < strlen($str); $a++) {
+    if ($str[$a] == 'a' OR $str[$a] == 'e' OR $str[$a] == 'i' OR $str[$a] == 'o' OR $str[$a] == 'u') {
+        $count++; 
+    }
+}
+echo "The number of vowels in $str are: " . $count;
 ?>
